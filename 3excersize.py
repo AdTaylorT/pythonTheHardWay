e458fbef1b6fcf31160507921412cbc027cd2a7a
unknown <adam.taylor.t@gmail.com>
2018-09-29 20:00:32 -0400

    completion of excersize 3

---
+++ 3excersize.py
@@ -0,0 +1,27 @@
+print("I will now count my chickens:")
+
+#Count the hens
+print("Hens", 25 + 30 / 6)
+# count the roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# count the eggs
+print("Now I will count the eggs:")
+# actual calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+# math question? results in a boolean operation
+print(3 + 2 < 5 - 7)
+
+#debugging the operation
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# oh yea. logic
+print("Oh, that's why it's False")
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
